d98ef5dd8db2d20b90a0733785500b52e700c003
like <1791773045@qq.com>
2023-09-24 09:37:09 +0800

    Corrected grammar errors in group-diagrams

--- group-anagrams/main.cpp
+++ group-anagrams/main.cpp
@@ -2,42 +2,35 @@
 #include <vector>
 
 using namespace std;
-/*给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。*/
 
-typedef char BTDataType;
+typedef int BTDataType;  // 修改数据类型为int
 
-typedef struct TreeNode
-{
+typedef struct TreeNode {
     BTDataType data;        // 当前节点值域
     struct TreeNode *left;  // 指向当前节点左孩子
     struct TreeNode *right; // 指向当前节点右孩子
 } BTNode;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> my_print;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helper(root, result);
+        return result;
+    }
 
-    vector<int> inorderTraversal(TreeNode *root)
-    {
-        if (root != NULL)
-        {
-            InOrder(root->left);
-            InOrder(root);
-            InOrder(root->right);
+private:
+    void helper(TreeNode* node, vector<int>& result) {
+        if (node) {
+            helper(node->left, result);
+            result.push_back(node->data);  // 将节点的值添加到结果向量中
+            helper(node->right, result);
         }
     }
 };
 
-void InOrder(TreeNode *note)
-{
-    cout<<&note<<endl;
-}
-
-
-int main()
-{
-    //明天起床验证并修订
-
+int main() {
+    // 明天起床验证并修订
+    printf("asds");
     return 0;
 }
