5f29056cdeb77158e7e720c14da59d1f74832268
like <like@xtourtec.com>
2023-09-25 15:18:07 +0800

    add reverse-linked-list

--- intersection-of-two-linked-lists/main.cpp
+++ intersection-of-two-linked-lists/main.cpp
@@ -3,75 +3,93 @@
 #include <unordered_map>
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        if(!headA || !headB) return nullptr;
-        
-        ListNode* a = headA;
-        ListNode* b = headB;
-        
-        while(a != b) {
-            a = a ? a->next : headB;
-            b = b ? b->next : headA;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        if (headA == nullptr || headB == nullptr)
+        {
+            return nullptr;
         }
-        return a;
+        ListNode *pA = headA, *pB = headB;
+        while (pA != pB)
+        {
+            pA = pA == nullptr ? headB : pA->next;
+            pB = pB == nullptr ? headA : pB->next;
+        }
+        return pA;
     }
 };
 
-int main() {
+int main()
+{
     int intersectVal, m, n, skipA, skipB;
     cout << "Enter intersectVal, m (nodes in listA), n (nodes in listB), skipA, skipB: ";
     cin >> intersectVal >> m >> n >> skipA >> skipB;
-    
+
     vector<int> valuesA(m), valuesB(n);
     cout << "Enter " << m << " values for List A: ";
-    for(int& val: valuesA) cin >> val;
+    for (int &val : valuesA)
+        cin >> val;
     cout << "Enter " << n << " values for List B: ";
-    for(int& val: valuesB) cin >> val;
+    for (int &val : valuesB)
+        cin >> val;
 
-    ListNode* common = nullptr;
-    ListNode* prev = nullptr;
-    for(int i = skipA; i < m; ++i) {
-        ListNode* node = new ListNode(valuesA[i]);
-        if(prev) prev->next = node;
-        if(!common) common = node;
+    ListNode *common = nullptr;
+    ListNode *prev = nullptr;
+    for (int i = skipA; i < m; ++i)
+    {
+        ListNode *node = new ListNode(valuesA[i]);
+        if (prev)
+            prev->next = node;
+        if (!common)
+            common = node;
         prev = node;
     }
 
-    ListNode* listA = nullptr;
+    ListNode *listA = nullptr;
     prev = nullptr;
-    for(int i = 0; i < skipA; ++i) {
-        ListNode* node = new ListNode(valuesA[i]);
-        if(prev) prev->next = node;
-        if(!listA) listA = node;
+    for (int i = 0; i < skipA; ++i)
+    {
+        ListNode *node = new ListNode(valuesA[i]);
+        if (prev)
+            prev->next = node;
+        if (!listA)
+            listA = node;
         prev = node;
     }
-    if(prev) prev->next = common;
+    if (prev)
+        prev->next = common;
 
-    ListNode* listB = nullptr;
+    ListNode *listB = nullptr;
     prev = nullptr;
-    for(int i = 0; i < skipB; ++i) {
-        ListNode* node = new ListNode(valuesB[i]);
-        if(prev) prev->next = node;
-        if(!listB) listB = node;
+    for (int i = 0; i < skipB; ++i)
+    {
+        ListNode *node = new ListNode(valuesB[i]);
+        if (prev)
+            prev->next = node;
+        if (!listB)
+            listB = node;
         prev = node;
     }
-    if(prev) prev->next = common;
-    
+    if (prev)
+        prev->next = common;
+
     Solution sol;
-    ListNode* intersection = sol.getIntersectionNode(listA, listB);
-    
-    if(intersection)
+    ListNode *intersection = sol.getIntersectionNode(listA, listB);
+
+    if (intersection)
         cout << "Intersected at '" << intersection->val << "'\n";
     else
         cout << "The lists do not intersect.\n";
-    
-
 }
---
+++ reverse-linked-list/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr; // 指向前一个节点的指针
+        ListNode* curr = head;    // 指向当前节点的指针
+        
+        while(curr != nullptr) {  // 当当前节点不为空时，继续遍历
+            ListNode* nextTemp = curr->next; // 暂存下一个节点
+            curr->next = prev;  // 将当前节点的 next 指针指向前一个节点，实现反转
+            prev = curr;    // 将前一个节点指针向后移动
+            curr = nextTemp; // 将当前节点指针向后移动
+        }
+        return prev;  // 当前节点指针到达链表末尾，返回前一个节点指针，即反转后的头节点
+    }
+
+    /*
+    解析：初始链表为1->2->3->4->5->null
+    此时prev为null，curr为1->2->3->4->5->null
+
+    第一轮：
+    nextTemp变成2->3->4->5->null
+    curr->next变成prev也就是null，那么curr就变成了1->null（因为curr是指向1的）
+    prev变成了curr，也就是prev变成了1->null
+    curr变成了nextTemp，也就是curr变成了2->3->4->5->null
+    综上所述：第一轮的结果是prev变成了1->null，curr变成了2->3->4->5->null
+
+    第二轮：
+    nextTemp变成3->4->5->null
+    curr->next变成prev也就是1->null，那么curr就变成了2->1->null（因为curr是指向2的）
+    prev变成了curr，也就是2->1->null
+    curr变成了nextTemp，也就是3->4->5->null
+    综上所述：第一轮的结果是prev变成了2->1->null，curr变成了3->4->5->null
+
+    以此类推...
+    返回prev即为答案  
+    */
+};
+
+
+int main()
+{
+    /*今后不再写int main*/
+    return 0;
+}
