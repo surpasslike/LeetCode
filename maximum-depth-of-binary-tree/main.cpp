330ee430632e7176f5032de240c93b33047af33f
like <1791773045@qq.com>
2023-09-26 20:07:33 +0800

    finish maximum-depth-of-binary-tree

--- maximum-depth-of-binary-tree/main.cpp
+++ maximum-depth-of-binary-tree/main.cpp
@@ -1,40 +1,66 @@
 #include <iostream>
+#include <algorithm> 
+#include <queue> 
 
 using namespace std;
 
-struct TreeNode
-{
+// 定义TreeNode
+struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-//TODO:未完成
-class Solution
-{
+// 你的Solution类
+class Solution {
 public:
-    int maxDepth(TreeNode *root)
-    {
-        int depth = 0;
-        if (!root)
-        {
-            return;
-        }
-
-        if (root->left != nullptr|| root->right != nullptr)
-        {
-            maxDepth(root->left);
-            maxDepth(root->right);
-            depth++;
-        }
-        return depth;
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
 
-int main()
-{
+// 辅助函数，用于将数组形式的树转化为实际的树结构
+TreeNode* createTree(const vector<int>& nums) {
+    if (nums.empty() || nums[0] == -1) return nullptr;
+    
+    TreeNode* root = new TreeNode(nums[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    int i = 1;
+    while (i < nums.size() && !q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+        
+        if (i < nums.size() && nums[i] != -1) {
+            node->left = new TreeNode(nums[i]);
+            q.push(node->left);
+        }
+        i++;
+        
+        if (i < nums.size() && nums[i] != -1) {
+            node->right = new TreeNode(nums[i]);
+            q.push(node->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
 
+int main() {
+    // 示例1
+    vector<int> nums1 = {3, 9, 20, -1, -1, 15, 7}; // -1表示null
+    TreeNode* root1 = createTree(nums1);
+    Solution s;
+    cout << "Example 1: " << s.maxDepth(root1) << " (Expected: 3)" << endl;
+
+    // 示例2
+    vector<int> nums2 = {1, -1, 2}; // -1表示null
+    TreeNode* root2 = createTree(nums2);
+    cout << "Example 2: " << s.maxDepth(root2) << " (Expected: 2)" << endl;
+
+    return 0;
 }
