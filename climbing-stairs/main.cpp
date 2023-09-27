9641c9e4158e3a55d06f853d40e165826b3e0219
like <like@xtourtec.com>
2023-09-27 18:18:09 +0800

    add Fibonacci sequence in climbing-stairs

--- climbing-stairs/main.cpp
+++ climbing-stairs/main.cpp
@@ -7,11 +7,19 @@ class Solution
 public:
     int climbStairs(int n)
     {
+        if (n < 3)
+        {
+            return n;
+        }
+        else
+        {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
     }
 };
 
 int main()
 {
-
+    
     return 0;
 }
