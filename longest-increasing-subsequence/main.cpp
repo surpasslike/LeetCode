ff43d65892335f5873958f149552a30210483896
like <like@xtourtec.com>
2023-09-28 15:15:06 +0800

    Add title description

--- longest-increasing-subsequence/main.cpp
+++ longest-increasing-subsequence/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+//最长递增子序列
 class Solution
 {
 public:
--- maximum-depth-of-binary-tree/main.cpp
+++ maximum-depth-of-binary-tree/main.cpp
@@ -3,7 +3,7 @@
 #include <queue> 
 
 using namespace std;
-
+//二叉树的最大深度
 // 定义TreeNode
 struct TreeNode {
     int val;
@@ -63,3 +63,24 @@ int main() {
 
     return 0;
 }
+
+/*
+给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+ 
+
+示例 1：
+
+
+
+ 
+
+输入：root = [3,9,20,null,null,15,7]
+输出：3
+示例 2：
+
+输入：root = [1,null,2]
+输出：2
+*/
