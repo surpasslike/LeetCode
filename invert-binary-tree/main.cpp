8c8626a3dddb1887ba02e957f4f364c429077612
like <like@xtourtec.com>
2023-10-07 20:22:32 +0800

    add invert-binary-tree

---
+++ invert-binary-tree/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+// 翻转二叉树
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            return nullptr;
+        }
+
+        else
+        {
+            return 
+        }
+    }
+};
