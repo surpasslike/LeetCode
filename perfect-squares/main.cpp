9713dc6a015a39bf64680ba6e8f9d3401c5288e3
like <like@xtourtec.com>
2023-10-17 17:50:53 +0800

    finish perfect-squares

--- perfect-squares/main.cpp
+++ perfect-squares/main.cpp
@@ -8,7 +8,7 @@ public:
     int numSquares(int n)
     {
         vector<int> f(n + 1);
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             int minn = INT_MAX;
             for (int j = 1; j * j <= i; j++)
