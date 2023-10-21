4c63ef154f8d76bff212a69750a00104f702c4ce
like <like@xtourtec.com>
2023-10-21 10:15:28 +0800

    add merge-intervals

---
+++ merge-intervals/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// 合并区间
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        
+    }
+};
