689cd8848f4f69966b14fe5677cc2e7dce233d3a
like <1791773045@qq.com>
2023-11-21 21:23:18 +0800

    finish

--- rotate-array/main.cpp
+++ rotate-array/main.cpp
@@ -2,20 +2,46 @@
 #include <vector>
 using namespace std;
 // 轮转数组
-
 class Solution
 {
 public:
     void rotate(vector<int> &nums, int k)
     {
+        int n = nums.size();
+        vector<int> newArr(n);
+        for (int i = 0; i < n; ++i)
+        {
+            newArr[(i + k) % n] = nums[i];
+        }
+        nums.assign(newArr.begin(), newArr.end());
     }
 };
 
+int main()
+{
+    // 创建 Solution 类的实例
+    Solution solution;
+
+    // 示例输入
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 2;
+
+    // 调用 rotate 方法进行轮转操作
+    solution.rotate(nums, k);
+
+    // 输出轮转后的结果
+    cout << "轮转后的结果: ";
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
 /*
 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
-
-
-
 示例 1:
 
 输入: nums = [1,2,3,4,5,6,7], k = 3
@@ -45,7 +71,3 @@ public:
 尽可能想出更多的解决方案，至少有 三种 不同的方法可以解决这个问题。
 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
 */
-== == == =
-}
-;
->>>>>>> de5412c788b1dd65124666dcb731d215bfad0435
